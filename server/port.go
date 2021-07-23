77472e02a5304240bf81bb3e12dac103a5d8d73d
snowmerak <go_dev@kakao.com>
2021-07-23 20:53:49 +0900

    Create port.go

---
+++ server/port.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"errors"
+	"net"
+	"strings"
+)
+
+func GetFreePort() (string, error) {
+	conn, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+	sp := strings.Split(conn.Addr().String(), ":")
+	if len(sp) < 2 {
+		return "", errors.New("not exist port")
+	}
+	return sp[1], nil
+}
